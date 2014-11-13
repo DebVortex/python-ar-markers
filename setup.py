fb89c57c762695008efac6b61c08a45443882d04
Max Brauer <max@transcode.de>
2014-11-13 22:41:10 +0100

    Added scripts section to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=['numpy', 'scipy'],
+    scripts=[
+        'ar_markers/scripts/ar_markers_generate_marker',
+        'ar_markers/scripts/ar_markers_livetest'
+    ],
     setup_requires=[],
     author='Max Brauer',
     author_email='max@max-brauer.com',
